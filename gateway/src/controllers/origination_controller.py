db40ff847e2027e91aa5b4f1feb0e81561c5a012
Кирилл Горюнов <kirillgorunov@MacBook-Pro-Kirill.local>
2024-06-26 15:46:32 +0300

    fix

--- gateway/src/controllers/origination_controller.py
+++ gateway/src/controllers/origination_controller.py
@@ -3,6 +3,8 @@ from fastapi.responses import JSONResponse
 
 import requests
 
+import httpx
+
 from config.settings import settings
 
 
@@ -14,9 +16,13 @@ async def close_application(agreement_id: int):
    '''
    Close application
    '''
-   try:
-      answer = requests.post(f"http://{settings.ORIGINATION_HOST}:{settings.ORIGINATION_PORT}/application/{agreement_id}/close")
-      answer_content = answer.json()
-      return JSONResponse(content={"message": answer_content}, status_code=answer.status_code)
-   except:
-      return JSONResponse(content={"message": "Servise not available"}, status_code=503)
+   link = f"http://{settings.ORIGINATION_HOST}:{settings.ORIGINATION_PORT}/application/{agreement_id}/close"
+   
+   async with httpx.AsyncClient(follow_redirects=True) as client:
+      try:
+         answer = await client.post(link)
+         answer.raise_for_status()
+         answer_content = answer.json()
+         return JSONResponse(content={"message": answer_content}, status_code=answer.status_code)
+      except httpx.RequestError:
+         return JSONResponse(content={"message": "Service not available"}, status_code=503)
--- gateway/src/controllers/product_engine_controllers.py
+++ gateway/src/controllers/product_engine_controllers.py
@@ -3,6 +3,8 @@ from fastapi.responses import JSONResponse
 
 import requests
 
+import httpx
+
 from config.settings import settings
 
 from common.shared_models.dto_models.request_model import RequestModel
@@ -16,36 +18,50 @@ async def get_products():
    '''
    Get all products
    '''
-   try:
-      answer = requests.get(f"http://{settings.PRODUCT_ENGINE_HOST}:{settings.PRODUCT_ENGINE_PORT}/product")
-      answer_content = answer.json()
-      return JSONResponse(content={"message": answer_content}, status_code=answer.status_code)
-   except:
-      return JSONResponse(content={"message": "Servise not available"}, status_code=503)
+   link = f"http://{settings.PRODUCT_ENGINE_HOST}:{settings.PRODUCT_ENGINE_PORT}/product"
+
+   async with httpx.AsyncClient(follow_redirects=True) as client:
+      try:
+         answer = await client.get(link)
+         answer.raise_for_status()
+         answer_content = answer.json()
+         return JSONResponse(content={"message": answer_content}, status_code=answer.status_code)
+      except httpx.RequestError:
+         return JSONResponse(content={"message": "Service not available"}, status_code=503)
+
 
 @pe_router.get("/product/{product_code}", tags=["product_engine"])
 async def get_product_by_code(product_code: str):
    '''
    Get product by code
    '''
-   try:
-      answer = requests.get(f"http://{settings.PRODUCT_ENGINE_HOST}:{settings.PRODUCT_ENGINE_PORT}/product/{product_code}")
-      answer_content = answer.json()
-      return JSONResponse(content={"message": answer_content}, status_code=answer.status_code)
-   except:
-      return JSONResponse(content={"message": "Servise not available"}, status_code=503)
+   link = f"http://{settings.PRODUCT_ENGINE_HOST}:{settings.PRODUCT_ENGINE_PORT}/product/{product_code}"
+
+   async with httpx.AsyncClient(follow_redirects=True) as client:
+      try:
+         answer = await client.get(link)
+         answer.raise_for_status()
+         answer_content = answer.json()
+         return JSONResponse(content={"message": answer_content}, status_code=answer.status_code)
+      except httpx.RequestError:
+         return JSONResponse(content={"message": "Service not available"}, status_code=503)
+
 
 @pe_router.post("/agreement", tags=["product_engine"])
 async def add_agreement(request: RequestModel):
    '''
    Add new agreement
    '''
-   try:
-      answer = requests.post(f"http://{settings.PRODUCT_ENGINE_HOST}:{settings.PRODUCT_ENGINE_PORT}/agreement", json=request.dict())
-      answer_content = answer.json()
-      return JSONResponse(content={"message": answer_content}, status_code=answer.status_code)
-   except:
-      return JSONResponse(content={"message": "Servise not available"}, status_code=503)
+   link = f"http://{settings.PRODUCT_ENGINE_HOST}:{settings.PRODUCT_ENGINE_PORT}/agreement"
+
+   async with httpx.AsyncClient(follow_redirects=True) as client:
+      try:
+         answer = await client.post(link, json=request.dict())
+         answer.raise_for_status()
+         answer_content = answer.json()
+         return JSONResponse(content={"message": answer_content}, status_code=answer.status_code)
+      except httpx.RequestError:
+         return JSONResponse(content={"message": "Service not available"}, status_code=503)
 
 
 @pe_router.get("/schedule/{agreement_id}", tags=["schedule"])
@@ -53,12 +69,16 @@ async def get_schedule(agreement_id: int):
    '''
    Get schedule by agreement_id
    '''
-   try:
-      answer = requests.get(f"http://{settings.PRODUCT_ENGINE_HOST}:{settings.PRODUCT_ENGINE_PORT}/schedule/{agreement_id}")
-      answer_content = answer.json()
-      return answer_content
-   except:
-      return JSONResponse(content={"message": "Servise not available"}, status_code=503)
+   link = f"http://{settings.PRODUCT_ENGINE_HOST}:{settings.PRODUCT_ENGINE_PORT}/schedule/{agreement_id}"
+
+   async with httpx.AsyncClient(follow_redirects=True) as client:
+      try:
+         answer = await client.get(link)
+         answer.raise_for_status()
+         answer_content = answer.json()
+         return JSONResponse(content={"message": answer_content}, status_code=answer.status_code)
+      except httpx.RequestError:
+         return JSONResponse(content={"message": "Service not available"}, status_code=503)
 
 
 @pe_router.get("/agreements/client/{client_id}", tags=["product_engine"])
@@ -66,9 +86,13 @@ async def get_agreements(client_id: int):
    '''
    Get agrements by clint_id
    '''
-   try:
-      answer = requests.get(f"http://{settings.PRODUCT_ENGINE_HOST}:{settings.PRODUCT_ENGINE_PORT}/agreement/client/{client_id}")
-      answer_content = answer.json()
-      return answer_content
-   except:
-      return JSONResponse(content={"message": "Servise not available"}, status_code=503)
+   link = f"http://{settings.PRODUCT_ENGINE_HOST}:{settings.PRODUCT_ENGINE_PORT}/agreement/client/{client_id}"
+
+   async with httpx.AsyncClient(follow_redirects=True) as client:
+      try:
+         answer = await client.get(link)
+         answer.raise_for_status()
+         answer_content = answer.json()
+         return JSONResponse(content={"message": answer_content}, status_code=answer.status_code)
+      except httpx.RequestError:
+         return JSONResponse(content={"message": "Service not available"}, status_code=503)
